df75194ccdf8583152fd1594ef2a1348eb5893f4
Joven Ong <joven.ong23@gmail.com>
2025-11-30 03:53:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4392536114-4d72f",
+  "appId": "1:678045416702:web:96597678802f19042cb9af",
+  "apiKey": "AIzaSyCG0-VRD83KgcriAE1hmAPkj23SI220XSI",
+  "authDomain": "studio-4392536114-4d72f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "678045416702"
+};
