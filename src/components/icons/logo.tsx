3038cf3c108180f0c8a3073db9bc8a20bcb8e895
Joven Ong <joven.ong23@gmail.com>
2025-12-01 03:19:49 +0000

    Here’s a comprehensive Firebase Studio prompt you can use to generate th

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,20 +3,23 @@ import type { SVGProps } from 'react';
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
     >
-        <path d="M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10z" />
-        <path d="M12.1 12.5a2.5 2.5 0 0 1-4.2 0" />
-        <path d="M16.1 12.5a2.5 2.5 0 0 1-4.2 0" />
-        <path d="M14.1 8.5a2.5 2.5 0 0 1-4.2 0" />
-        <path d="M9 17a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2H9v-2z" />
+        <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"/>
+        <path d="M15.44 16.34c.9-.88 1.43-2.03 1.56-3.24" />
+        <path d="M12 13.5c-1.6 0-3-1.3-3-3s1.4-3 3-3c.3 0 .7.1 1 .2" />
+        <path d="m8.56 16.34c-.9-.88-1.43-2.03-1.56-3.24" />
+        <path d="M12 22c1.657 0 3-1.79 3-4" />
+        <path d="M12 22c-1.657 0-3-1.79-3-4" />
+        <path d="m14 7 3-2" />
+        <path d="m10 7-3-2" />
     </svg>
   );
 }
